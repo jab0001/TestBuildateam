ba153419244aae3f33fbe2b9af938b48ae384db4
Pavel <jab001@yandex.ru>
2019-08-15 17:27:36 +0300

    доработал html под спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -90,8 +90,13 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html"));
-  gulp.watch("source/*.html", gulp.series("html"));
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh" , function(done) {
+  server.reload();
+  done();
 });
 
 gulp.task("build", gulp.series("clean","copy","css","images","webpimg","sprite","html"));
