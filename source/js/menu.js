c542288cbe80731a783137baaf7b2541275cb40c
Pavel <Jab001@ya.ru>
2019-08-23 15:40:03 +0300

    вынес меню и попуп в отдельные js файлы

---
+++ source/js/menu.js
@@ -0,0 +1,10 @@
+var menuButton = document.querySelector(".main-nav__toggle"),
+    menu = document.querySelector(".main-nav");
+
+
+
+menuButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  menu.classList.toggle("main-nav--closed");
+  menu.classList.toggle("main-nav--opened");
+})
---
+++ source/js/popup.js
@@ -0,0 +1,28 @@
+var popupButton = document.querySelector(".top-sale__button , .manufacturing__button"),
+    popup = document.querySelector(".modal"),
+    popupFog = document.querySelector(".fog");
+
+
+popupButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal--active");
+  popupFog.classList.add("fog--active");
+})
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal--active")) {
+      popup.classList.remove("modal--active");
+      popupFog.classList.remove("fog--active");
+    }
+  }
+})
+
+popupFog.addEventListener("click", function (evt) {
+  evt.preventDefault();
+    if (popup.classList.contains("modal--active")) {
+      popup.classList.remove("modal--active");
+      popupFog.classList.remove("fog--active");
+  }
+})
