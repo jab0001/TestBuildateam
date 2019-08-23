9540805edb5a4512db8dd26f55b2416ff93ff520
Pavel <Jab001@ya.ru>
2019-08-23 15:08:38 +0300

    Js + modal по центру

--- source/js/shit.js
+++ source/js/shit.js
@@ -1,28 +1,28 @@
-var popupWriteLink = document.querySelector(".top-sale__button , .manufacturing__button"),
-    popupWrite = document.querySelector(".modal"),
+var popupButton = document.querySelector(".top-sale__button , .manufacturing__button"),
+    popup = document.querySelector(".modal"),
     popupFog = document.querySelector(".fog"),
-    slideMenuButton = document.querySelector(".main-nav__toggle"),
-    slideMenu = document.querySelector(".main-nav");
+    menuButton = document.querySelector(".main-nav__toggle"),
+    menu = document.querySelector(".main-nav");
 
 
 
-slideMenuButton.addEventListener("click", function (evt) {
+menuButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  slideMenu.classList.toggle("main-nav--closed");
-  slideMenu.classList.toggle("main-nav--opened");
+  menu.classList.toggle("main-nav--closed");
+  menu.classList.toggle("main-nav--opened");
 })
 
-popupWriteLink.addEventListener("click", function (evt) {
+popupButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popupWrite.classList.add("modal--active");
+  popup.classList.add("modal--active");
   popupFog.classList.add("fog--active");
 })
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popupWrite.classList.contains("modal--active")) {
-      popupWrite.classList.remove("modal--active");
+    if (popup.classList.contains("modal--active")) {
+      popup.classList.remove("modal--active");
       popupFog.classList.remove("fog--active");
     }
   }
@@ -30,8 +30,8 @@ window.addEventListener("keydown", function (evt) {
 
 popupFog.addEventListener("click", function (evt) {
   evt.preventDefault();
-    if (popupWrite.classList.contains("modal--active")) {
-      popupWrite.classList.remove("modal--active");
+    if (popup.classList.contains("modal--active")) {
+      popup.classList.remove("modal--active");
       popupFog.classList.remove("fog--active");
   }
 })
